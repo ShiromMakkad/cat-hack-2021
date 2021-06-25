4b268df403efb9b303da71d91f6721c1c72ca197
Preston Chao <prestonchao@Prestons-MacBook-Pro.local>
2021-06-25 10:37:07 -0500

    small change to dropdown labels

--- src/components/drop-down/drop-down.js
+++ src/components/drop-down/drop-down.js
@@ -2,8 +2,7 @@ import React from 'react'
 import { DropdownButton, Dropdown } from 'react-bootstrap'
 
 const DropDownComp = () => (
-    <DropdownButton id="dropdown-basic-button" title="Select Country">
-        <Dropdown.Item href="#/action-1">US</Dropdown.Item>
+    <DropdownButton id="dropdown-basic-button" title="US">
         <Dropdown.Item href="#/action-2">UK</Dropdown.Item>
         <Dropdown.Item href="#/action-3">China</Dropdown.Item>
         <Dropdown.Item href="#/action-3">India</Dropdown.Item>
